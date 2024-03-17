b73f916171722fc4f5a943199983f58013f04e0b
Habiba <160347457+Habiba-Wasim@users.noreply.github.com>
2024-03-17 22:35:50 +0500

    Step 23 of Typescript
    Step 23 of Typescript

---
+++ Q23.js
@@ -0,0 +1,24 @@
+"use strict";
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True otherwise I predict False")
+console.log(car == 'subaru');
+console.log(car == 'civic');
+let num = 23;
+//console.log("Is num == 23 (in number)  ? I predict true otherwise I predict False")
+console.log(num === 23);
+console.log(num === "23");
+let Name = 'hiba';
+console.log(Name === 'hiba');
+console.log(Name === 'wasim');
+let name1 = 'Mango';
+console.log(name1 === 'Mango');
+console.log(name1 === 'Apple');
+let day = 'Tuesday';
+console.log(day === 'Tuesday');
+console.log(day === 'Sunday');
